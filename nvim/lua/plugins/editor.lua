fbb4b0f93d5420c11c4837530688a81b6dc91a5c
MaxB-Coder <maxblaschek72@gmail.com>
2026-02-21 21:08:50 +0000

    add custom WezTerm icon in orange Claude theme

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,6 +3,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
     config = function()
       require("telescope").setup()
     end,
