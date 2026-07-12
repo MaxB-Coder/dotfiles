8648b72529d81ce6d39944e2ccdb45486aeccb13
MaxB-Coder <theburnerrrrrr@gmail.com>
2026-07-12 13:01:12 +0000

    nvim: OSC52 clipboard for SSH sessions

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -38,3 +38,16 @@ vim.lsp.config("lua_ls", {
     },
   },
 })
+
+-- Add copy/paste over ssh
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
